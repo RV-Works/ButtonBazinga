a4d985d799a5ee4620b0d347476d6bd14c01c5da
RV-Works <robinavalkenburg@gmail.com>
2026-04-23 10:06:47 +0200

    Feat: Player
    Movement, Jump

---
+++ ButtonBazinga/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 1f;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    public bool isGrounded;
+    [SerializeField] private int jumpHeight;
+    [SerializeField] private float groundCheckDistance = 0.35f;
+    [SerializeField] private LayerMask groundMask = ~0;
+    [SerializeField] private float maxGroundSlopeAngle = 55f;
+
+    [SerializeField] private float fallGravityMultiplier = 20f;
+
+    private CapsuleCollider capsuleCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = moveInput.x * transform.right + moveInput.y * transform.forward;
+        Debug.Log(moveInput);
+        rb.AddForce(direction * playerSpeed, ForceMode.VelocityChange);
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        else { return; }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
